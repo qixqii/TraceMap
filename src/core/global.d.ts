8ded2675a431ea3918147666f71ad3321369e7a6
阿逼 <bbnocode@outlook.com>
2024-07-31 09:34:24 +0800

    feat(index.ts & global.d.ts):add methods

--- src/core/global.d.ts
+++ src/core/global.d.ts
@@ -1,3 +1,6 @@
 interface Console {
-  info(...args:any[]):void
+  info(...args: any[]): void
+  method(...args: any[]): void
+  obj(...args: any[]): void
+  error(...args: any[]):void
 }
--- src/core/index.ts
+++ src/core/index.ts
@@ -11,10 +11,8 @@ const showLog = () => {
 
 
 /**
- * 
  * @param args any[]
  * @returns void
- * @example 
  */
 console.info = (...args: any[]) => {
   if (!showLog()) return;
@@ -26,3 +24,36 @@ console.info = (...args: any[]) => {
   );
 }
 
+console.method = (...args: any[]) => {
+  if (!showLog()) return
+  console.log(
+    `${publicPrefix} %c[ ${getTime()} ] [M] %c󰡱`,
+    "color: #8300ff; padding: 2px;",
+    "color: #8300ff; font-size: 23px;",
+    ...args
+  );
+}
+
+console.obj = (...args: any[]) => {
+  if (!showLog()) return
+  console.log(
+      `${publicPrefix} %c[ ${getTime()} ] [O] %c󰘦`,
+      "color: #ff5f00; padding: 2px; ",
+      "color: #ff5f00; font-size: 23px;",
+      ...args.map((obj) => JSON.parse(JSON.stringify(obj)))
+  );
+}
+
+console.error = (...args: any[]) => {
+  if (!showLog()) return;
+  console.log(
+    `${publicPrefix} %c 这是一个大大的提示!`,
+    "color: #6670EE; font-size: 88px;text-shadow: -1px -1px 0 #000,  1px -1px 0 #000, -1px  1px 0 #000,1px  1px 0 #000; "
+  );
+  console.log(
+    `${publicPrefix} %c[ ${getTime()} ] [E] %c `,
+    "color: red; padding: 2px;",
+    "color: red; font-size: 15px;",
+    ...args
+  );
+}
